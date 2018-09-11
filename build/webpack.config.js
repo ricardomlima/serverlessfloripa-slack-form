2140d5dc7290969781cc2be59672995fad292891
Ricardo M. Lima <ricardolima89@gmail.com>
2018-09-11 01:50:26 -0300

    html webpack plugin with template & dev server

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -1,9 +1,22 @@
-const path = require('path')
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/app.js',
+    entry: {
+        bundle: './src/app.js'
+    },
     output: {
-        filename : 'bundle.js',
         path: path.resolve(__dirname, '../dist')
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Join Serverless Floripa Slack channel',
+            template: 'src/index.html'
+        })
+    ],
+    devServer: {
+        port:3000,
+        open: true
     }
 }

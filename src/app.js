ec14776455805d8078ebf47edea1bdb5e121b717
Ricardo M. Lima <ricardolima89@gmail.com>
2018-09-16 23:59:20 -0300

    Preparing form request

--- src/app.js
+++ src/app.js
@@ -1,2 +1,20 @@
-import 'bootstrap';
 import './app.css';
+
+var form = document.querySelector('form')
+
+function submitForm(evt){
+  evt.preventDefault();
+  sendInviteRequest();
+}
+
+function sendInviteRequest(){
+  var submittedEmail = document.getElementById('slack-email').value;
+  var request = new XMLHttpRequest();
+  var formData = { email: submittedEmail };
+  var formattedFormData = JSON.stringify(formData);
+
+  request.open('POST', '/invite');
+  request.send(formattedFormData);
+}
+
+form.addEventListener('submit', submitForm);

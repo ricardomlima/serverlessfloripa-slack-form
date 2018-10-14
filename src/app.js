0d29eae92d5b8c26fc967466f0b3f40732e39264
Ricardo M. Lima <ricardolima89@gmail.com>
2018-10-13 21:53:20 -0300

    Server processing behaviour

--- src/app.js
+++ src/app.js
@@ -13,9 +13,19 @@ function sendInviteRequest(){
   const formData = { email: submittedEmail };
   const formattedFormData = JSON.stringify(formData);
 
+  form.classList.add('loading');
   request.open('POST', 'https://us-central1-serverlessfloripa.cloudfunctions.net/handleInviteRequest');
   request.setRequestHeader('Content-Type', 'application/json')
+  request.onreadystatechange = handleInviteResponse
+  request.onreadystatechange = function(){handleInviteResponse(request)}
   request.send(formattedFormData);
+}
+
+
+function handleInviteResponse(request){
+  if (request.readyState === 4){
+    form.classList.remove('loading');
+  }
 }
 
 form.addEventListener('submit', submitForm);
